29ff2ea1611395bf122969076c7853cca56fab1e
Hector <norzagaray@me.com>
2017-11-29 18:24:46 -0800

    added shorter solution code

--- insertdashes/solution.js
+++ insertdashes/solution.js
@@ -1,13 +1,13 @@
 function insertDash(number) {
  let previous='',
-	 newstring='';
+     current= '',
+	   newstring='';
 
 	number = number.toString();
 	for ( i=0; i<number.length; i++){
 		parseInt(number[i])%2 === 0? current='even' : current='odd';
 		if (current==='odd' && previous==='odd'){
 			newstring=newstring + '-' + number[i];
-			previous='odd';
 		} else {
 			newstring=newstring + number[i];
 		 	previous=current;
